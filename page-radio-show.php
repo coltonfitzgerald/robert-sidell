6fcdb7432772078763f2ab5db9aa603d8a71a8af
Brandon Ly <brandon@brandonly.me>
2015-09-26 22:21:58 -0700

    updated radio page

--- page-radio-show.php
+++ page-radio-show.php
@@ -27,7 +27,7 @@ get_header(); ?>
 		  				</audio> 
 	  				</div>
 	  				<div class="broadcast-description">
-	  					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tellus lectus, interdum vel convallis ac, mollis sed nisl. Suspendisse et augue vel sem congue auctor non quis risus. Pellentesque a velit quis arcu commodo consequat. Sed mauris nulla, volutpat ut risus sit amet, pellentesque sodales purus. Nunc blandit elit euismod egestas ornare. Maecenas ipsum ipsum, molestie at porttitor ac, tempor vitae enim. Nunc sodales vitae ante eget sagittis. Praesent tempor in ante id tempor. Suspendisse sodales convallis leo, ut tristique felis lacinia nec. Nulla a urna quis leo facilisis sollicitudin. Suspendisse vitae diam a leo posuere eleifend. Sed interdum, nisi vel commodo sagittis, nulla lectus vehicula metus, eget vehicula mi tortor vel augue. In venenatis finibus risus, id varius neque viverra ac. Donec sed ultrices sem. </p>
+	  					<p></p>
 	  				</div>
   				</div>
 			</div>
