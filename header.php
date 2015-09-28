781e8448eb20fb0b5a5b29f20085bf579def794f
Brandon Ly <brandon@brandonly.me>
2015-09-27 22:05:36 -0700

    add mobile menu to header

--- header.php
+++ header.php
@@ -33,6 +33,7 @@
 			</div>
 		</div>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<div class="mobile-menu"><?php wp_nav_menu( array( 'theme_location' => 'mobile', 'menu_id' => 'mobile-menu' ) ); ?></div>
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'robert-sidell' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
