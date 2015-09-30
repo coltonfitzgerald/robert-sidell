a3f23458e79879e7322925ae09fdf969aa83f8c2
Brandon Ly <brandon@brandonly.me>
2015-09-29 23:55:40 -0700

    added videos to testimonial page

--- page-testimonials.php
+++ page-testimonials.php
@@ -10,6 +10,16 @@ get_header(); ?>
 			<div class="testimonials-page">
 				<h1 class="page-title">Testimonials</h1>
 				<?php echo do_shortcode("[show-testimonials orderby='menu_order' order='ASC' layout='grid' options='theme:none,info-position:info-above,text-alignment:left,columns:2,rating:on,date:on,quote-content:short,charlimitextra: (...),image-size:ttshowcase_small,image-shape:circle,image-effect:none,image-link:on']"); ?>
+				<div class="testimonial-video">
+					<video controls>
+						<source src="<?php echo get_site_url(); ?>/wp-content/themes/robert-sidell/img/Testimonial-1.mp4" type="video/mp4">
+					</video>
+				</div>
+				<div class="testimonial-video">
+					<video controls>
+						<source src="<?php echo get_site_url(); ?>/wp-content/themes/robert-sidell/img/Testimonial-2.mp4" type="video/mp4">
+					</video>
+				</div>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
