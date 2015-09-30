f2debf250e32ea086a70a963dee606a2d3d3819a
Brandon Ly <brandon@brandonly.me>
2015-09-29 21:02:04 -0700

    added shortcode to testmonials page

---
+++ page-testimonials.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	Template Name: Testimonials Page
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="testimonials-page">
+				<?php echo do_shortcode("[show-testimonials orderby='menu_order' order='ASC' layout='grid' options='theme:none,info-position:info-above,text-alignment:left,columns:2,rating:on,date:on,quote-content:short,charlimitextra: (...),image-size:ttshowcase_small,image-shape:circle,image-effect:none,image-link:on']"); ?>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
