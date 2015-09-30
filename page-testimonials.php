2bba071909491ac5fffb269671f65e7f2d1defc8
Brandon Ly <brandon@brandonly.me>
2015-09-29 23:48:47 -0700

    added testimonial title

--- page-testimonials.php
+++ page-testimonials.php
@@ -8,6 +8,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="testimonials-page">
+				<h1 class="page-title">Testimonials</h1>
 				<?php echo do_shortcode("[show-testimonials orderby='menu_order' order='ASC' layout='grid' options='theme:none,info-position:info-above,text-alignment:left,columns:2,rating:on,date:on,quote-content:short,charlimitextra: (...),image-size:ttshowcase_small,image-shape:circle,image-effect:none,image-link:on']"); ?>
 			</div>
 		</main><!-- #main -->
