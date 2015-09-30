d7b4f8da18e9aabd4e8e72a1258bacdbfad89d18
Brandon Ly <brandon@brandonly.me>
2015-09-30 16:29:51 -0700

    prepare for slicknav

--- header.php
+++ header.php
@@ -36,9 +36,9 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<div class="mobile-menu">
 				<div class="hamburger-menu">
-					<svg width="70px" fill="black" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></path></svg>
+					<svg width="35px" fill="black" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></path></svg>
 				</div>
-				<div class="mobile-wrap">
+				<div class="mobile-wrap" id="menu">
 					<?php wp_nav_menu( array( 'theme_location' => 'mobile', 'menu_id' => 'mobile-menu' ) ); ?>
 				</div>
 			</div>
--- js/menu.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    $(".hamburger-menu").click(function() {
-    	$(".mobile-wrap").toggle("slow");
-    }); 
-    $(".mobile-wrap").click(function() {
-    	$(".mobile-wrap").hide("slow");
-    });
-});
